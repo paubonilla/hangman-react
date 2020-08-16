610bac44bca118bf3e78cd6d729cb1ccf0ba5799
Pau Bonilla <paulo111995@gmail.com>
2020-08-15 19:29:25 -0700

    Updates

---
+++ precache-manifest.38950b24f371dce0fafa5b3c525a3049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9ef2fba8919e466f5fab88757e1cdc",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "5bf6d8788c74e36441aa",
+    "url": "/hangman-react/static/css/main.628f1ee5.chunk.css"
+  },
+  {
+    "revision": "fb970f20cc2af18575b4",
+    "url": "/hangman-react/static/js/2.0b8bb35a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hangman-react/static/js/2.0b8bb35a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf6d8788c74e36441aa",
+    "url": "/hangman-react/static/js/main.1abd7a7f.chunk.js"
+  },
+  {
+    "revision": "3f7c69ac9d5e568f51ba",
+    "url": "/hangman-react/static/js/runtime-main.0e2bf36f.js"
+  }
+]);
